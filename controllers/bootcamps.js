087c10672300768c1338333e06ba37e50ef0a9e6
gauravgraj <venkatgraj91@gmail.com>
2019-10-26 00:22:40 +0530

    updated controllers and routes file.

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,7 +1,6 @@
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
-
 exports.getBootcamps = (req, res, next) => {
   res.status(200).json({ success: true, msg: 'Show all bootcamps' });
 };
@@ -9,7 +8,31 @@ exports.getBootcamps = (req, res, next) => {
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps/:id
 // @access  Public
-
 exports.getBootcamp = (req, res, next) => {
   res.status(200).json({ success: true, msg: `Show bootcamp ${req.param.id}` });
+};
+
+// @desc    Create bootcamps
+// @route   POST /api/v1/bootcamp
+// @access  Private
+exports.createBootcamp = (req, res, next) => {
+  res.status(200).json({ success: true, msg: 'Create new bootcamp' });
+};
+
+// @desc    Update bootcamp
+// @route   PUT /api/v1/bootcamps/:id
+// @access  Private
+exports.updateBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
+};
+
+// @desc    Delete all bootcamps
+// @route   PUT /api/v1/bootcamps/:id
+// @access  Private
+exports.deleteBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
 };
